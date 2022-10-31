36dcef7069a4d44fa8de022bbf74b1f9efbb79f7
xcfokor <h.oguzhangok@gmail.com>
2022-10-31 13:25:13 +0300

    Text-based game started, the game need to continue

--- classes.py
+++ classes.py
@@ -272,75 +272,209 @@ class Merchant():
         self.weapon_point= self.starting_weapon_point
         self.armor_point= self.starting_armor_point
         print("Profit:",self.revenue)
+
+def start():
+    print("Welcome to The Game\n")
+    print("Please define your Orcs name:")
+    orcs_name=input()
+    ad=orcs_name
+    orcs_name=Orcs(50,30,150,10)
+    print("Your Orcs' name is: ",ad, "and properties are:")
+    orcs_name.Print()
     
-savasci= Orcs(50,30,150,10)#defining objects
-navin=Dwarves(40,50,150,15)
-joker=Dwarves(60,50,80,15)
-thor=Elves(45,35,100,5)
-demirci=Merchant()
+    print("Please define your Elves name:")
+    elves_name=input()
+    ad=elves_name
+    elves_name=Elves(45,35,100,5)
+    print("Your Elves' name is: ",ad,"and properties are:")
+    elves_name.Print()
+
+    print("Please define your Dwarves name:")
+    dwarves_name=input()
+    ad=dwarves_name
+    dwarves_name=Dwarves(40,50,150,15)
+    print("Your Dwarves' name is: ",ad,"and properties are:")
+    dwarves_name.Print()
+
+    print("Please define your Merchant name:")
+    merchant_name=input()
+    ad=merchant_name
+    merchant_name=Merchant()
+    print("Your Merchant's name is: ",ad, "and revenue is :")
+    merchant_name.Print()
+
+    
+    
+    def action_selection(orcs_name,dwarves_name,elves_name,merchant_name):
+        print("------Day Begin------\n","Choose an Action: \n","1   Print Faction information\n","2   Sell weapons\n","3   Sell armors\n","4   End the day\n","5   End the game\n","6   Quit Game\n")
+        inp=input()
+        if inp=="1":
+            orcs_name.Print()
+            print("\n")
+            dwarves_name.Print()
+            print("\n")
+            elves_name.Print()
+            print("\n")
+            merchant_name.Print()
+            print("\n")
+            return action_selection(orcs_name,dwarves_name,elves_name,merchant_name)
+        elif inp=="2":
+            print("Sell weapon to who?")
+            print("Choose a Faction: \n","1   Orcs\n","2   Dwarves\n","3   Elves\n","4   All\n","5   Back\n")
+            select=input()
+            if select=="1":
+                print("Choose Weapon Point:")
+                point=input()
+                orcs_name.PurchaseWeapons(int(point),merchant_name)
+                
+            elif select=="2":
+                print("Choose Weapon Point:")
+                point=input()
+                dwarves_name.PurchaseWeapons(int(point),merchant_name)
+            elif select=="3":
+                print("Choose Weapon Point:")
+                point=input()
+                elves_name.PurchaseWeapons(int(point),merchant_name)
+            elif select=="4":
+                print("Choose Weapon Point:")
+                point=input()
+                dwarves_name.PurchaseWeapons(int(point),merchant_name)
+                elves_name.PurchaseWeapons(int(point),merchant_name)
+                orcs_name.PurchaseWeapons(int(point),merchant_name)
+            elif select=="5":
+                return action_selection(orcs_name,dwarves_name,elves_name,merchant_name)
+            else :
+                print("Plesase choose again")
+                return action_selection(orcs_name,dwarves_name,elves_name,merchant_name)
+
+        elif inp=="3":
+            print("Sell armor to who?")
+            print("Choose a Faction: \n","1   Orcs\n","2   Dwarves\n","3   Elves\n","4   All\n","5   Back\n")
+            select=input()
+            if select=="1":
+                print("Choose Armor Point:")
+                point=input()
+                orcs_name.PurchaseArmors(int(point),merchant_name)
+            elif select=="2":
+                print("Choose Armor Point:")
+                point=input()
+                dwarves_name.PurchaseArmors(int(point),merchant_name)
+            elif select=="3":
+                print("Choose Armor Point:")
+                point=input()
+                elves_name.PurchaseArmors(int(point),merchant_name)
+            elif select=="4":
+                print("Choose Armor Point:")
+                point=input()
+                dwarves_name.PurchaseArmors(int(point),merchant_name)
+                elves_name.PurchaseArmors(int(point),merchant_name)
+                orcs_name.PurchaseArmors(int(point),merchant_name)
+            elif select=="5":
+                return action_selection(orcs_name,dwarves_name,elves_name,merchant_name)
+            else :
+                print("Plesase choose again")
+                return action_selection(orcs_name,dwarves_name,elves_name,merchant_name)
+
+        elif inp=="4":
+            print("End of the day")
+            orcs_name.EndTurn()
+            print("\n")
+            dwarves_name.EndTurn()
+            print("\n")
+            elves_name.EndTurn()
+            print("\n")
+            merchant_name.EndTurn()
+            print("\n")
+            
+        elif inp=="5":
+            print("End of the game")
+            orcs_name.EndTurn()
+            print("\n")
+            dwarves_name.EndTurn()
+            print("\n")
+            elves_name.EndTurn()
+            print("\n")
+            merchant_name.EndTurn()
+            print("\n")
+            
+            return quit()
+        elif inp=="6":
+            print("Quitting the game")
+
+            return quit()
+            
+    action_selection(orcs_name,dwarves_name,elves_name,merchant_name)
 
 
-navin.AssgnEnemies(savasci, thor)#Assign Enemies
-thor.AssgnEnemies(savasci,navin)
-thor.AssgnEnemies(navin,joker)
-savasci.AssgnEnemies(navin,thor)
-joker.AssgnEnemies(savasci,thor)
-
-print("BAşlangıç")
-savasci.Print()
-thor.Print()
-navin.Print()
-joker.Print()
-demirci.Print()
+start()
+# savasci= Orcs(50,30,150,10)#defining objects
+# navin=Dwarves(40,50,150,15)
+# joker=Dwarves(60,50,80,15)
+# thor=Elves(45,35,100,5)
+# demirci=Merchant()
 
 
-navin.PerformAttack()
-thor.PerformAttack()
-savasci.PerformAttack()
-navin.PerformAttack()
-thor.PerformAttack()
-savasci.PerformAttack()
-navin.PerformAttack()
-thor.PerformAttack()
-savasci.PerformAttack()
-joker.PerformAttack()
+# navin.AssgnEnemies(savasci, thor)#Assign Enemies
+# thor.AssgnEnemies(savasci,navin)
+# thor.AssgnEnemies(navin,joker)
+# savasci.AssgnEnemies(navin,thor)
+# joker.AssgnEnemies(savasci,thor)
 
-joker.Print()
-savasci.Print()
-thor.Print()
-navin.Print()
-demirci.Print()
+# print("BAşlangıç")
+# savasci.Print()
+# thor.Print()
+# navin.Print()
+# joker.Print()
+# demirci.Print()
 
-savasci.EndTurn()
-thor.EndTurn()
-navin.EndTurn()
-demirci.EndTurn()
-joker.EndTurn()
 
-print("satın alımlar")
+# navin.PerformAttack()
+# thor.PerformAttack()
+# savasci.PerformAttack()
+# navin.PerformAttack()
+# thor.PerformAttack()
+# savasci.PerformAttack()
+# navin.PerformAttack()
+# thor.PerformAttack()
+# savasci.PerformAttack()
+# joker.PerformAttack()
 
-savasci.PurchaseWeapons(2,demirci)
-thor.PurchaseArmors(5,demirci)
-thor.PurchaseWeapons(6,demirci)
-navin.PurchaseWeapons(7,demirci)
+# joker.Print()
+# savasci.Print()
+# thor.Print()
+# navin.Print()
+# demirci.Print()
 
-savasci.Print()
-thor.Print()
-navin.Print()
-demirci.Print()
+# savasci.EndTurn()
+# thor.EndTurn()
+# navin.EndTurn()
+# demirci.EndTurn()
+# joker.EndTurn()
 
-navin.PerformAttack()
-thor.PerformAttack()
-savasci.PerformAttack()
+# print("satın alımlar")
 
-savasci.EndTurn()
-thor.EndTurn()
-navin.EndTurn()
-demirci.EndTurn()
+# savasci.PurchaseWeapons(2,demirci)
+# thor.PurchaseArmors(5,demirci)
+# thor.PurchaseWeapons(6,demirci)
+# navin.PurchaseWeapons(7,demirci)
 
-print("2.tur bitti")
-savasci.Print()
-thor.Print()
-navin.Print()
-demirci.Print()
+# savasci.Print()
+# thor.Print()
+# navin.Print()
+# demirci.Print()
+
+# navin.PerformAttack()
+# thor.PerformAttack()
+# savasci.PerformAttack()
+
+# savasci.EndTurn()
+# thor.EndTurn()
+# navin.EndTurn()
+# demirci.EndTurn()
+
+# print("2.tur bitti")
+# savasci.Print()
+# thor.Print()
+# navin.Print()
+# demirci.Print()
 
